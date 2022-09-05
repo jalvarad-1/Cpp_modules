43d2680847c26f53ad805d67d53333f4a9a576c4
jalvarad-1 <jalvarad-1@github.com>
2022-09-05 16:35:58 +0200

    creo que está acabado el cpp08

--- cpp08/ex02/mutantstack.hpp
+++ cpp08/ex02/mutantstack.hpp
@@ -6,34 +6,39 @@
 /*   By: jalvarad <jalvarad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 14:06:04 by jalvarad          #+#    #+#             */
-/*   Updated: 2022/09/04 18:04:44 by jalvarad         ###   ########.fr       */
+/*   Updated: 2022/09/05 16:19:44 by jalvarad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MUTANT_STACK_HPP
-#define MUTANT_STACK_HPP
-#include <vector>
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
 #include <iostream>
-#include <algorithm>
+#include <deque>
 #include <stack>
 template<typename T>
 class MutantStack: public std::stack<T>
 {
-	typedef std::stack<T>::container_type::iterator iterator;
-	typedef std::stack<T>::container_type::const_iterator const_iterator;
-	typedef std::stack<T>::container_type::reverse_iterator reverse_iterator;
-	typedef std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
-
 	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
 		MutantStack(void): std::stack<T>(){}
-		MutantStack(const MutantStack & src): std::stack<T>(src){return *this = this;};
-		MutantStack & operator=(const MutantStack & src) 
+		MutantStack(const MutantStack & src): std::stack<T>(src){return *this = this;}
+		MutantStack & operator=(const MutantStack & src)
 		{
-			this->std::stack<T>operator= src;
+			this->std::stack<T>::operator= (src);
 			return *this;
 		}
-		~MutantStack(void){};
-		iterator begin(){return c.begin();}
-		iterator end(){return c.end();}
-}
+		~MutantStack(void){}
+		iterator begin(){return this->c.begin();}
+		iterator end(){return this->c.end();}
+		reverse_iterator rbegin(){return this->c.rbegin();}
+		reverse_iterator rend(){return this->c.rend();}
+		const_iterator cbegin(){return this->c.cbegin();}
+		const_iterator cend(){return this->c.crend();}
+		const_reverse_iterator crbegin(){return this->c.crbegin();}
+		const_reverse_iterator crend(){return this->c.crend();}
+};
 #endif
